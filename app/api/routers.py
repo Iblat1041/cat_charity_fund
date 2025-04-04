8ea65d6d2768ce94cfd5e47d6d773ff61584d8bd
ibalt <brezhev@ya.ru>
2025-04-04 13:05:20 +0300

    api

---
+++ app/api/routers.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from .endpoints import charity_router, donation_router, user_router
+
+main_router = APIRouter()
+main_router.include_router(user_router)
+main_router.include_router(charity_router, prefix="/charity_project", tags=[
+    "Charity Projects"
+])
+main_router.include_router(donation_router, prefix="/donation", tags=[
+    "Donations"
+])
